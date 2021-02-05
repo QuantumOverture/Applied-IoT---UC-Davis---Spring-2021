589c7d28212739671ff8b643b76b6844cff37cf9
ismaildude <38566647+ismaildude@users.noreply.github.com>
2021-02-04 19:00:09 -0800

    Add files via upload

---
+++ ReadTempAndSend.py
@@ -0,0 +1,15 @@
+import Adafruit_DHT
+from time import sleep
+sensor = Adafruit_DHT.DHT11
+DHT11_pin = 4
+
+while(True):
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, DHT11_pin)
+    
+    if humidity is not None and temperature is not None:
+      print('Temperature={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+    else:
+      print('Failed to get reading from the sensor. Try again!')
+      exit(1)
+
+    sleep(5)
