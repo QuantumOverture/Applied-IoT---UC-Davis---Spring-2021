fbc6975dcb3d6063e5b9da1c93c4b26460562889
ismaildude <38566647+ismaildude@users.noreply.github.com>
2021-04-15 10:34:47 -0700

    Update AltWeek3Code.py

--- AltWeek3Code.py
+++ AltWeek3Code.py
@@ -18,7 +18,7 @@ while True:
         RH = dhtDevice.humidity
         heatindex = heatindex = -42.379 + (2.04901523*T) + (10.14333127*RH) - (0.22475541*T*RH) - (0.00683783*T*T) - (0.05481717*RH*RH) + (0.00122874*T*T*RH) + (0.0$
         JSONData = "{{ temperature:{},humidity:{},heatindex:{} }}".format(T,RH,heatindex)
-        requests.post("https://webhook.site/b87fd34f-f7dd-4c30-9a0e-4a2c5682a837",data=JSONData,headers={'Content-type':"application/json","Accept":"text/plain"})
+        requests.post("<WEBHOOK URL HERE>",data=JSONData,headers={'Content-type':"application/json","Accept":"text/plain"})
         print(JSONData)
     except RuntimeError as error:
         # Errors happen fairly often, DHT's are hard to read, just keep going
