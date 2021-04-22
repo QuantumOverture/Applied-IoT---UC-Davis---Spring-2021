83f4466c659a4b2369d43de06de5e09a2d0e488e
ismaildude <38566647+ismaildude@users.noreply.github.com>
2021-04-21 23:40:39 -0700

    Update SendJSONData.py

--- SendJSONData.py
+++ SendJSONData.py
@@ -16,7 +16,7 @@ while(True):
         T = (temperature * (9/5)) + 32
         RH = humidity
         heatindex = -42.379 + (2.04901523*T) + (10.14333127*RH) - (0.22475541*T*RH) - (0.00683783*T*T) - (0.05481717*RH*RH) + (0.00122874*T*T*RH) + (0.00085282*T*RH*RH) - (0.00000199*T*T*RH*RH)
-        JSONData  = '{{ temperature:{}, humidity:{}, heatindex:{} }}'.format(temperature, humidity,heatindex)
+        JSONData  = '{{ "temperature":{}, "humidity":{}, "heatindex":{} }}'.format(temperature, humidity,heatindex)
         requests.post("<Enter web hook URL here>",data=JSONData,headers={"Content-type":"application/json","Accept":"text/plain"})
         print(JSONData)
     else:
