c5d672afd1c08890640ef46f77062adbb426374f
ismaildude <38566647+ismaildude@users.noreply.github.com>
2021-05-03 19:01:52 -0700

    Add files via upload

---
+++ Week#6/ReadAndRespond.py
@@ -0,0 +1,27 @@
+
+import Adafruit_DHT
+from time import sleep
+import requests
+sensor = Adafruit_DHT.DHT11
+DHT11_pin = 18
+import json
+
+while(True):
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, DHT11_pin)
+    if humidity is not None and temperature is not None:
+        T = (temperature * (9/5)) + 32
+        RH = humidity
+        heatindex = -42.379 + (2.04901523*T) + (10.14333127*RH) - (0.22475541*T*RH) - (0.00683783*T*T) - (0.05481717*RH*RH) + (0.00122874*T*T*RH) + (0.00085282*T*RH$
+        JSONData  = '{{ "temperature":{}, "humidity":{},"heatindex":{} }}'.format(temperature, humidity,heatindex)
+        print(JSONData)
+        SigResponse = requests.post("http://f97bd0d2b752.ngrok.io",data=JSONData,headers={"Content-type":"application/json","Accept":"test/plain"})
+        JSONResponse = json.loads(SigResponse.text)
+        if JSONResponse["Z-Score"] > 1.96 or JSONResponse["Z-Score"] < -1.96:
+            print("Significant")
+        else:
+            print("InSignificant")
+    else:
+      print('Failed to get reading from the sensor. Try again!')
+      exit(1)
+
+    sleep(3)
---
+++ Week#6/Week6Server.py
@@ -0,0 +1,42 @@
+from flask import Flask,request
+import json
+from flask_sqlalchemy import SQLAlchemy
+from flask import render_template
+from scipy.stats import zmap
+
+
+app = Flask(__name__)
+
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/<USERNAME>/mysite/APPDATA.sqlite3'
+
+database = SQLAlchemy(app)
+class TempHumidityHeatIndex(database.Model):
+    SendID = database.Column(database.Integer, primary_key = True)
+    Temp = database.Column(database.Integer)
+    HeatIndex = database.Column(database.Integer)
+    Humidity = database.Column(database.Integer)
+
+    def __init__(self, TempSent, HeatIndexSent, HumiditySent):
+        self.Temp = TempSent
+        self.HeatIndex = HeatIndexSent
+        self.Humidity = HumiditySent
+
+
+@app.route('/',methods=["POST","GET"])
+def StoreOrDisplayData():
+    if request.method == "POST":
+        JsonData = json.loads(request.data)
+        print(JsonData)
+        database.session.add(TempHumidityHeatIndex(JsonData["temperature"],JsonData["heatindex"],JsonData["humidity"]))
+        database.session.commit()
+
+        # You would not do the following in production:
+        # You would have all the elements of the z-score calculation "cached" and only change it when needed
+        return '{{ "Z-Score":{} }}'.format(zmap(JsonData["heatindex"],[RowObj.HeatIndex for RowObj in TempHumidityHeatIndex.query.all()])[0])
+    elif request.method == "GET":
+        return render_template("index.html",WeatherData = TempHumidityHeatIndex.query.all())
+
+if __name__ == '__main__':
+    database.create_all()
+    app.run()
+                                                                                                                  
