03acd775523cf8d9e10f2858a9324b236175f3f4
ismaildude <38566647+ismaildude@users.noreply.github.com>
2021-05-03 19:41:07 -0700

    Update ReadAndRespond.py

--- Week#6/ReadAndRespond.py
+++ Week#6/ReadAndRespond.py
@@ -11,7 +11,7 @@ while(True):
     if humidity is not None and temperature is not None:
         T = (temperature * (9/5)) + 32
         RH = humidity
-        heatindex = -42.379 + (2.04901523*T) + (10.14333127*RH) - (0.22475541*T*RH) - (0.00683783*T*T) - (0.05481717*RH*RH) + (0.00122874*T*T*RH) + (0.00085282*T*RH$
+        heatindex = -42.379 + (2.04901523*T) + (10.14333127*RH) - (0.22475541*T*RH) - (0.00683783*T*T) - (0.05481717*RH*RH) + (0.00122874*T*T*RH) + (0.00085282*T*RH*RH) - (0.00000199*T*T*RH*RH)
         JSONData  = '{{ "temperature":{}, "humidity":{},"heatindex":{} }}'.format(temperature, humidity,heatindex)
         print(JSONData)
         SigResponse = requests.post("http://f97bd0d2b752.ngrok.io",data=JSONData,headers={"Content-type":"application/json","Accept":"test/plain"})
