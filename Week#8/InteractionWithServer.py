77d27f34b3f0bdbdb019f3ee91703c26de740b75
ismaildude <38566647+ismaildude@users.noreply.github.com>
2021-05-19 11:39:15 -0700

    Add files via upload

--- Week#8/InteractionWithServer.py
+++ Week#8/InteractionWithServer.py
@@ -1,6 +1,24 @@
 import requests
 
 
-JSONData = '{"heatindex":78.1,"error":-1,"correctlabel":-1}'
 
-requests.post("",data=JSONData,headers={"Content-type":"application/json","Accept":"test/plain"})
+# Change the url to your deployed AWS server
+print(requests.post("http://127.0.0.1:5000/",data='{"heatindex":78.1,"error":-1,"correctlabel":-1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
+
+
+# Error check
+print(requests.post("http://127.0.0.1:5000/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
+print(requests.get("http://127.0.0.1:5000/").text)
+
+
+# 9 more errors to force training
+print(requests.post("http://127.0.0.1:5000/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
+print(requests.post("http://127.0.0.1:5000/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
+print(requests.post("http://127.0.0.1:5000/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
+print(requests.post("http://127.0.0.1:5000/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
+print(requests.post("http://127.0.0.1:5000/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
+print(requests.post("http://127.0.0.1:5000/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
+print(requests.post("http://127.0.0.1:5000/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
+print(requests.post("http://127.0.0.1:5000/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
+print(requests.post("http://127.0.0.1:5000/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
+print(requests.post("http://127.0.0.1:5000/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
