e6220f708ed166271a93192262790a8b4d92adca
ismaildude <38566647+ismaildude@users.noreply.github.com>
2021-05-19 17:48:39 -0700

    Add files via upload

--- Week#8/InteractionWithServer.py
+++ Week#8/InteractionWithServer.py
@@ -1,48 +1,9 @@
 import requests
 
 
-
 # Change the url to your deployed AWS server
-print(requests.post("http://iotsmartfanserver-env-2.eba-r5pwu882.us-east-2.elasticbeanstalk.com/",data='{"heatindex":78.1,"error":-1,"correctlabel":-1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
-
+print(requests.post("<Your website>",data='{"heatindex":78.1,"error":-1,"correctlabel":-1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
 
 # Error check
-print(requests.post("http://iotsmartfanserver-env-2.eba-r5pwu882.us-east-2.elasticbeanstalk.com/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
-
-print(requests.post("http://iotsmartfanserver-env-2.eba-r5pwu882.us-east-2.elasticbeanstalk.com/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
-
-print(requests.post("http://iotsmartfanserver-env-2.eba-r5pwu882.us-east-2.elasticbeanstalk.com/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
-
-print(requests.post("http://iotsmartfanserver-env-2.eba-r5pwu882.us-east-2.elasticbeanstalk.com/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
-
-print(requests.post("http://iotsmartfanserver-env-2.eba-r5pwu882.us-east-2.elasticbeanstalk.com/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
-
-print(requests.post("http://iotsmartfanserver-env-2.eba-r5pwu882.us-east-2.elasticbeanstalk.com/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
-
-print(requests.post("http://iotsmartfanserver-env-2.eba-r5pwu882.us-east-2.elasticbeanstalk.com/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
-
-print(requests.post("http://iotsmartfanserver-env-2.eba-r5pwu882.us-east-2.elasticbeanstalk.com/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
-
-print(requests.post("http://iotsmartfanserver-env-2.eba-r5pwu882.us-east-2.elasticbeanstalk.com/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for i in range(1,12):
+    print(requests.post("<Your website>",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
