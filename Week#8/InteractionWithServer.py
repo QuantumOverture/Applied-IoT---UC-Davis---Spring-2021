d933f5695bf731eeb1d470dbf463ffffa103cbe7
ismaildude <38566647+ismaildude@users.noreply.github.com>
2021-05-19 17:09:32 -0700

    Add files via upload

--- Week#8/InteractionWithServer.py
+++ Week#8/InteractionWithServer.py
@@ -3,22 +3,46 @@ import requests
 
 
 # Change the url to your deployed AWS server
-print(requests.post("http://127.0.0.1:5000/",data='{"heatindex":78.1,"error":-1,"correctlabel":-1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
+print(requests.post("http://iotsmartfanserver-env-2.eba-r5pwu882.us-east-2.elasticbeanstalk.com/",data='{"heatindex":78.1,"error":-1,"correctlabel":-1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
 
 
 # Error check
-print(requests.post("http://127.0.0.1:5000/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
-print(requests.get("http://127.0.0.1:5000/").text)
+print(requests.post("http://iotsmartfanserver-env-2.eba-r5pwu882.us-east-2.elasticbeanstalk.com/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
+
+print(requests.post("http://iotsmartfanserver-env-2.eba-r5pwu882.us-east-2.elasticbeanstalk.com/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
+
+print(requests.post("http://iotsmartfanserver-env-2.eba-r5pwu882.us-east-2.elasticbeanstalk.com/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
+
+print(requests.post("http://iotsmartfanserver-env-2.eba-r5pwu882.us-east-2.elasticbeanstalk.com/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
+
+print(requests.post("http://iotsmartfanserver-env-2.eba-r5pwu882.us-east-2.elasticbeanstalk.com/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
+
+print(requests.post("http://iotsmartfanserver-env-2.eba-r5pwu882.us-east-2.elasticbeanstalk.com/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
+
+print(requests.post("http://iotsmartfanserver-env-2.eba-r5pwu882.us-east-2.elasticbeanstalk.com/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
+
+print(requests.post("http://iotsmartfanserver-env-2.eba-r5pwu882.us-east-2.elasticbeanstalk.com/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
+
+print(requests.post("http://iotsmartfanserver-env-2.eba-r5pwu882.us-east-2.elasticbeanstalk.com/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
 
 
-# 9 more errors to force training
-print(requests.post("http://127.0.0.1:5000/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
-print(requests.post("http://127.0.0.1:5000/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
-print(requests.post("http://127.0.0.1:5000/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
-print(requests.post("http://127.0.0.1:5000/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
-print(requests.post("http://127.0.0.1:5000/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
-print(requests.post("http://127.0.0.1:5000/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
-print(requests.post("http://127.0.0.1:5000/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
-print(requests.post("http://127.0.0.1:5000/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
-print(requests.post("http://127.0.0.1:5000/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
-print(requests.post("http://127.0.0.1:5000/",data='{"heatindex":78.1,"error":1,"correctlabel":1}',headers={"Content-type":"application/json","Accept":"test/plain"}).text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
